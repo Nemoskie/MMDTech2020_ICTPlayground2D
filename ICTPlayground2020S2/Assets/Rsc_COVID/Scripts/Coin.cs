17e06102bb90c396052da95397e9076a4088d154
Nemoskie <43193073+Nemoskie@users.noreply.github.com>
2020-06-07 17:51:31 +0700

    3D platform map
    MAP, no sound, not finish

---
+++ ICTPlayground2020S2/Assets/Rsc_COVID/Scripts/Coin.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coin : MonoBehaviour
+{
+
+   
+    void OnTriggerEnter(Collider collider)
+    {
+        if (!collider.CompareTag("Player"))
+        {
+            return;
+        }
+        ++PlayerUnit.CoinsCollected;
+        Destroy(gameObject);
+    }
+}
